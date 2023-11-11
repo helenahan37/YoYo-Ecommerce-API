ffc8942b9175e9e39690362d1b269f3508e3c0c0
Helena.Han <hlyhelena37@gmail.com>
2023-11-11 19:45:51 +1100

    set db

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const app = require('./app/app.js');
 const http = require('http');
+const { databaseConnect } = require('./config/dbConnect');
 
 //create a server
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 //listen server
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+	await databaseConnect();
 	console.log(`Server is running on port ${PORT}`);
 });
