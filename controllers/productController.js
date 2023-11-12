de3d5ad59d300db173d75330e003b3c11c8481ba
Helena.Han <hlyhelena37@gmail.com>
2023-11-12 19:10:01 +1100

    feat: pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -62,15 +62,45 @@ const getProducts = asyncHandler(async (req, res) => {
 	if (req.query.price) {
 		//split the price range
 		const priceRange = req.query.price.split('-');
-
 		//query the price range greater than or equal to the first value and less than or equal to the second value
 		productQuery = productQuery.find({ price: { $gte: priceRange[0], $lte: priceRange[1] } });
+	}
+
+	//pagination
+	// if user do not specify page number, default to 1
+	const page = parseInt(req.query.page) || 1;
+	// if user do not specify limit, default to 10 records per page
+	const limit = parseInt(req.query.limit) || 10;
+	// start index
+	const startIndex = (page - 1) * limit;
+	// end index
+	const endIndex = page * limit;
+	// get the total number of products
+	const total = await Product.countDocuments();
+	// pagination result
+	productQuery = productQuery.skip(startIndex).limit(limit);
+
+	const pagination = {};
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit,
+		};
+	}
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit,
+		};
 	}
 
 	const products = await productQuery;
 
 	res.json({
 		status: 'success',
+		total,
+		results: products.length,
+		pagination,
 		products,
 	});
 });
